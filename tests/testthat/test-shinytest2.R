d7f8e8344cb864c8517ac2bd314d42f497d73427
Rich Bielby <richbielby@e.email>
2026-04-14 16:07:02 +0100

    Updating test snapshots

--- tests/testthat/test-shinytest2.R
+++ tests/testthat/test-shinytest2.R
@@ -5,17 +5,30 @@ test_that("{shinytest2} recording: english-and-maths-progress-matrix", {
     test_path("../.."),
     name = "english-and-maths-progress-matrix",
     height = 1305, width = 2259,
-    load_timeout = 320 * 60,
-    timeout = 320 * 60
+    load_timeout = 360 * 1000,
+    timeout = 360 * 1000
   )
+
+  app$wait_for_idle(500)
   app$expect_values()
-  app$set_inputs(navlistPanel = "matrix_dashboard")
+
+  app$set_inputs(navlistPanel = "matrix_dashboard", wait_ = FALSE)
+  app$wait_for_idle(500)
   app$expect_values()
-  app$set_inputs(dropdown_academicyr = "2024/25")
+
+  app$set_inputs(dropdown_academicyr = "2024/25", wait_ = FALSE)
+  app$wait_for_idle(500)
   app$expect_values()
-  app$set_inputs(dropdown_subject = "English")
+
+  app$set_inputs(dropdown_subject = "English", wait_ = FALSE)
+  app$wait_for_idle(500)
   app$expect_values()
-  app$set_inputs(dropdown_subject = "Maths")
-  app$set_inputs(dropdown_sex = "Female")
+
+  app$set_inputs(
+    dropdown_subject = "Maths",
+    dropdown_sex = "Female",
+    wait_ = FALSE
+  )
+  app$wait_for_idle(500)
   app$expect_values()
 })
