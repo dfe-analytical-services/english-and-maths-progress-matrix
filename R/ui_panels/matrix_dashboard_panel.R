a80d1bcfef473cd7a7cdfb226513ed01ca679696
Rich Bielby <richbielby@e.email>
2026-04-14 17:19:07 +0100

    Clean up around layout, download button and header

--- R/ui_panels/matrix_dashboard_panel.R
+++ R/ui_panels/matrix_dashboard_panel.R
@@ -15,68 +15,52 @@ matrix_dashboard_panel <- function() {
         # input selection --------------------------------------------------
         column(
           width = 12,
-          gov_row(
-            column(
-              width = 6,
-              selectizeInput(
-                inputId = "dropdown_academicyr",
-                label = "Select an academic year:",
-                choices <- paste0(
-                  substr(sort(unique(raw_data$academic_year)), 1, 4),
-                  "/",
-                  substr(
-                    sort(unique(raw_data$academic_year)),
-                    5,
-                    nchar(sort(unique(raw_data$academic_year)))
-                  )
+          layout_columns(
+            selectizeInput(
+              inputId = "dropdown_academicyr",
+              label = "Academic year:",
+              choices <- paste0(
+                substr(sort(unique(raw_data$academic_year)), 1, 4),
+                "/",
+                substr(
+                  sort(unique(raw_data$academic_year)),
+                  5,
+                  nchar(sort(unique(raw_data$academic_year)))
                 )
               )
             ),
-            column(
-              width = 6,
-              selectizeInput(
-                inputId = "dropdown_subject",
-                label = "Choose a subject:",
-                choices <- choices_subject
-              )
+            selectizeInput(
+              inputId = "dropdown_subject",
+              label = "Subject:",
+              choices <- choices_subject
             ),
-            column(
-              width = 6,
-              selectizeInput(
-                inputId = "dropdown_sex",
-                label = "Choose a sex:",
-                choices <- raw_data %>%
-                  select(sex) %>%
-                  distinct() %>%
-                  pull() %>%
-                  sort(.)
-              )
+            selectizeInput(
+              inputId = "dropdown_sex",
+              label = "Sex:",
+              choices <- raw_data %>%
+                select(sex) %>%
+                distinct() %>%
+                pull() %>%
+                sort(.)
             ),
-            column(
-              width = 6,
-              # checkboxInput(
-              # inputId = "select_colour",
-              # label = "Display table in colour?",
-              # value = TRUE
-              selectizeInput(
-                inputId = "select_colour",
-                label = "Display table in colour?",
-                choices <- choices_colour
-              )
+            # checkboxInput(
+            # inputId = "select_colour",
+            # label = "Display table in colour?",
+            # value = TRUE
+            selectizeInput(
+              inputId = "select_colour",
+              label = "Display in colour?",
+              choices <- choices_colour
             )
-          ),
-          # data download --------------------------------------------------
-          gov_row(
-            column(
-              width = 12,
-              paste("Download the underlying data for this dashboard:"),
-              br(),
-              downloadButton(
-                outputId = "download_data",
-                label = "Download data",
-                icon = NULL,
-                class = "gov-uk-button-secondary"
-              )
+          )
+        ),
+        # data download --------------------------------------------------
+        gov_row(
+          column(
+            width = 12,
+            download_button(
+              outputId = "download_data",
+              button_label = "Download underlying data"
             )
           )
         ),
@@ -84,7 +68,7 @@ matrix_dashboard_panel <- function() {
         gov_row(
           column(
             width = 12,
-            h2(textOutput("reactive_matrix_title_out")),
+            tags$h2(textOutput("reactive_matrix_title_out"), class = "govuk-heading-m"),
             gov_row(
               column(
                 width = 12,
